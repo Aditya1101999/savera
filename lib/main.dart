6f3d1077fe5820a0f3a39654e38b1b7b15b5c40c
adi <adi854105@gmail.com>
2023-03-31 01:08:09 +0530

    final changes

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,13 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
+
+
 
 class _HomePageState extends State<HomePage> {
   String message = '';
@@ -37,8 +40,7 @@ class _HomePageState extends State<HomePage> {
               Container(
                 margin: const EdgeInsets.only(top: 32.0),
                 child: Row(
-                  children: [
-                  ],
+                  children: [],
                 ),
               ),
               Expanded(
@@ -47,26 +49,62 @@ class _HomePageState extends State<HomePage> {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     GestureDetector(
-                      child: Image.asset(
-                        'assets/images/up.png',
-                        height: 150.0,
-                        width: 150.0,
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 5000),
+                        curve: Curves.easeInOut,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: message == 'Good touch'
+                                ? Colors.green
+                                : Colors.transparent,
+                            width: 4.0,
+                          ),
+                          borderRadius: BorderRadius.circular(75.0),
+                        ),
+                        child: Image.asset(
+                          'assets/images/up.png',
+                          height: 150.0,
+                          width: 150.0,
+                        ),
                       ),
                       onTap: () {
                         setState(() {
                           message = 'Good touch';
                         });
+                        Future.delayed(Duration(milliseconds: 5000), () {
+                          setState(() {
+                            message = '';
+                          });
+                        });
                       },
                     ),
                     GestureDetector(
-                      child: Image.asset(
-                        'assets/images/down.png',
-                        height: 150.0,
-                        width: 150.0,
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 5000),
+                        curve: Curves.easeInOut,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: message == 'Bad touch'
+                                ? Colors.red
+                                : Colors.transparent,
+                            width: 4.0,
+                          ),
+                          borderRadius: BorderRadius.circular(75.0),
+                        ),
+                        child: Image.asset(
+                          'assets/images/down.png',
+                          height: 150.0,
+                          width: 150.0,
+                        ),
                       ),
                       onTap: () {
                         setState(() {
                           message = 'Bad touch';
+                        });
+                        Future.delayed(Duration(milliseconds: 500), () {
+                          setState(() {
+                            message = '';
+                          });
                         });
                       },
                     ),
@@ -99,7 +137,10 @@ class _HomePageState extends State<HomePage> {
                     ),
                     const Spacer(),
                     IconButton(
-                      icon: const Icon(Icons.info_outline),
+                      icon: const Icon(
+                        Icons.info_outline,
+                        color: Colors.grey,
+                      ),
                       onPressed: () {},
                     ),
                     const SizedBox(width: 32.0),
@@ -113,21 +154,73 @@ class _HomePageState extends State<HomePage> {
             left: 32.0,
             child: ElevatedButton.icon(
               onPressed: () {},
-              icon: Icon(Icons.login),
-              label: Text('Login'),
+              icon: Icon(
+                Icons.login,
+                color: Colors.white,
+              ),
+              label: Text(
+                'Login',
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ElevatedButton.styleFrom(
+                // ignore: deprecated_member_use
+                primary: Colors.red,
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+              ),
             ),
           ),
           Positioned(
             top: 32.0,
-            right: 32.0,
-            child: ElevatedButton.icon(
-              onPressed: () {},
-              icon: Icon(Icons.connect_without_contact),
-              label: Text('Connect'),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+right: 32.0,
+child: ElevatedButton.icon(
+onPressed: () {},
+icon: Icon(
+Icons.connect_without_contact,
+color: Colors.white,
+),
+label: Text(
+'Connect',
+style: TextStyle(color: Colors.white),
+),
+style: ElevatedButton.styleFrom(
+backgroundColor: Colors.red,
+padding: EdgeInsets.symmetric(horizontal: 16.0),
+shape: RoundedRectangleBorder(
+borderRadius: BorderRadius.circular(16.0),
+),
+),
+),
+),
+if (message.isNotEmpty)
+Positioned(
+bottom: 32.0,
+left: 0,
+right: 0,
+child: Container(
+padding: const EdgeInsets.symmetric(
+horizontal: 16.0,
+vertical: 8.0,
+),
+decoration: BoxDecoration(
+color: message == 'Good touch' ? Colors.green : Colors.red,
+borderRadius: BorderRadius.circular(16.0),
+),
+child: Text(
+message,
+textAlign: TextAlign.center,
+style: TextStyle(
+color: Colors.white,
+fontWeight: FontWeight.bold,
+fontSize: 24.0,
+),
+),
+),
+),
+],
+),
+);
+}
 }
